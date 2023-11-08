5d2cf422dffe7b681bee9bc3def47be1e7a96312
Aamirah Patel <aamirah.patel@baesystems.com>
2023-11-08 13:30:12 +0000

    switch statement for weather image

--- src/Components/WeatherData.tsx
+++ src/Components/WeatherData.tsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
+import {WiCloud} from 'react-icons/wi'
+
 
 const WeatherApp = () => {
   const [city, setcity] = useState('London');
-  const [weatherData, setWeatherData] = useState('');
+  const [weatherData, setWeatherData] = useState({});
+  const [Wicon, setWicon] = useState(<WiCloud/>)
 
   const search = async () => {
     if (city ==="") {
       return;
-    
+    }
       try { 
         const response = await fetch (`/api/weather?city=${city}`);
         if (response.status === 200) {
           const data = await response.json();
           setWeatherData(data);
 
+        } else {
+          alert ("Location not found or invalid input.");
         }
+
+      } catch (error) {
+        console.error(error);
+        alert("An error occured while fetching data")
       }
-    }
   }
 }
 
--- src/Components/WeatherImage.tsx
+++ src/Components/WeatherImage.tsx
@@ -1,9 +1,48 @@
-import {TiWeatherSunny} from 'react-icons/ti'
+import { useState, useEffect } from 'react';
+import {WiCloudy, WiDaySprinkle, WiDayRain, WiSnow, WiDaySunny} from 'react-icons/wi';
+
+const [Wicon, setWicon] = useState(<WiDaySunny/>)
+const [weatherData, setWeatherData] = useState({});
 
 const WeatherImage = () => {
   return (
-    <TiWeatherSunny size={"100"} color="orange"/>
+    if (weatherData && weatherData.main && weatherData.main.temp !== undefined) {
+        switch (weatherData.weather[0].icon) {
+            case "01":
+            case "01n":
+                break;
+            case "02d":
+            case "012n":
+                setWicon(<WiCloudy/>);
+                break;
+            case "03d":
+            case "03n":
+            case "04d":
+            case "04n":
+                setWicon(<WiDaySprinkle/>);
+                break;
+            case "09d":
+            case "09n":
+                setWicon(<WiDayRain/>);
+                break;
+            case "13d":
+            case "13n":
+                setWicon(<WiSnow/>);
+                break;
+            default:
+                setWicon(<WiDaySunny/>);
+        }
+    }
   )
 }
+
+useEffect(() => {
+    updateWeatherIcon();
+}, [weatherData]);
+
+useEffect(() => {
+    // Fetch data for the default city when the component mounts
+    search();
+}, []);
 
 export default WeatherImage
